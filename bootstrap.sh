5a14b4749a571c719184b4504df463bd80814090
abs <abs.pathak@ticketmaster.com>
2014-10-20 23:08:23 -0700

    Updated bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,77 +21,79 @@ DISTRO=""
 grep "ubuntu" /etc/apt/sources.list && DISTRO="ubuntu" 
 
 if [ "$DISTRO" == "" ];then 
-	echo "Could not determine OS distribution: $DISTRO";
-	exit 1;
+    echo "Could not determine OS distribution: $DISTRO";
+    exit 1;
 else
-	echo "Distribution: $DISTRO";
+    echo "Distribution: $DISTRO";
 fi
 
 install_nginx_rpm() {
     ## DISTRO: centos, oracle, rhel
-	rpm -qa | grep 'nginx-release' || { 
-		yum -y install "${NGINX_PKG_URL}/${DISTRO}/6/noarch/RPMS/nginx-release-${DISTRO}-6-0.el6.ngx.noarch.rpm" && yum -y install nginx;
-		chkconfig nginx on;
-	}
+    rpm -qa | grep 'nginx-release' || { 
+        yum -y install "${NGINX_PKG_URL}/${DISTRO}/6/noarch/RPMS/nginx-release-${DISTRO}-6-0.el6.ngx.noarch.rpm" && yum -y install nginx;
+        chkconfig nginx on;
+    }
 }
 
 install_nginx_deb() {
-	
-	## TODO: prompt and read from stdin.
-	# echo Please enter $DISTRO codename:
-	CODENAME=${1:-"trusty"};
+    
+    ## TODO: prompt and read from stdin.
+    # echo Please enter $DISTRO codename:
+    CODENAME=${1:-"trusty"};
 
-	SOURCES_LIST="/etc/apt/sources.list";
-	
+    SOURCES_LIST="/etc/apt/sources.list";
+    
     ## Add nginx repository
-	apt-key add "http://nginx.org/keys/nginx_signing.key";
-	grep "$NGINX_SRC_URL" $SOURCES_LIST || { 
-	    echo -e "\ndeb ${NGINX_PKG_URL}/${DISTRO}/ ${CODENAME} nginx\ndeb-src ${NGINX_PKG_URL}/${DISTRO}/ ${CODENAME} nginx\n" >> $SOURCES_LIST;
-	    apt-get update;
+    apt-key add "http://nginx.org/keys/nginx_signing.key";
+    grep "$NGINX_SRC_URL" $SOURCES_LIST || { 
+        echo -e "\ndeb ${NGINX_PKG_URL}/${DISTRO}/ ${CODENAME} nginx\ndeb-src ${NGINX_PKG_URL}/${DISTRO}/ ${CODENAME} nginx\n" >> $SOURCES_LIST;
+        apt-get update;
     }
     ## Install nginx
     apt-get install -y nginx;
 }
 
 install_nginx() {
-	if [[ ( "$DISTRO" == "centos" ) || ( "$DISTRO" == "oracle" ) || ( "$DISTRO" == "rhel" ) || ( "$DISTRO" == "redhat" ) ]]; then
-		install_nginx_rpm;
-	else
-		install_nginx_deb;
-	fi
-	[ -f "${NGINX_CONF_DIR}/default.conf" ] && mv ${NGINX_CONF_DIR}/default.conf ${NGINX_CONF_DIR}/default.conf.disabled;
+    if [[ ( "$DISTRO" == "centos" ) || ( "$DISTRO" == "oracle" ) || ( "$DISTRO" == "rhel" ) || ( "$DISTRO" == "redhat" ) ]]; then
+        install_nginx_rpm;
+    else
+        install_nginx_deb;
+    fi
+    [ -f "${NGINX_CONF_DIR}/default.conf" ] && mv ${NGINX_CONF_DIR}/default.conf ${NGINX_CONF_DIR}/default.conf.disabled;
 }
 
 bootstrap_metrilyx_rpm() {
-	for PKG in $RPM_PKGS; do
-		rpm -qa | grep $PKG || yum -y install $PKG
-	done
-}	
+    for PKG in $RPM_PKGS; do
+        rpm -qa | grep $PKG || yum -y install $PKG
+    done
+}   
 
 bootstrap_metrilyx_deb() {
-	apt-get install -y $DEB_PKGS;
+    apt-get install -y $DEB_PKGS;
 }
 
 bootstrap_metrilyx() {
-	if [[ ( "$DISTRO" == "centos" ) || ( "$DISTRO" == "oracle" ) || ( "$DISTRO" == "rhel" ) || ( "$DISTRO" == "redhat" ) ]]; then
-		bootstrap_metrilyx_rpm;
-	    which pip || easy_install pip;
-	    pip install "numpy>=1.6.1";    
-	else
-		bootstrap_metrilyx_deb;
-	    which pip || easy_install pip;
-	fi
+    if [[ ( "$DISTRO" == "centos" ) || ( "$DISTRO" == "oracle" ) || ( "$DISTRO" == "rhel" ) || ( "$DISTRO" == "redhat" ) ]]; then
+        bootstrap_metrilyx_rpm;
+        which pip || easy_install pip;
+        pip install "numpy>=1.6.1";    
+    else
+        bootstrap_metrilyx_deb;
+        which pip || easy_install pip;
+    fi
 }
 
 install_metrilyx() {
-	pip install git+https://github.com/Ticketmaster/metrilyx-2.0.git
+    pip install git+https://github.com/Ticketmaster/metrilyx-2.0.git
 }
+
+
+#### Main ####
 
 install_nginx;
 bootstrap_metrilyx;
 
 if [ "$1" == "install" ]; then
-	echo " * Installing Metrilyx...";
-	install_metrilyx;
+    echo " * Installing Metrilyx...";
+    install_metrilyx;
 fi
-
